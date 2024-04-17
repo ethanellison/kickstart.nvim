3aba1dbddfd03ef1c28f623335e8c3842f7f8769
Damjan 9000 <damjan.9000@gmail.com>
2024-04-17 21:25:54 +0200

    Move plugin examples from README to optional plugin files (#831)
    * Move autopairs example from README to an optional plugin
    
    * Move neo-tree example from README to an optional plugin

--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -1,5 +1,15 @@
+<<<<<<< HEAD:lua/custom/plugins/autopairs.lua
+
 return {
   'windwp/nvim-autopairs',
+=======
+-- autopairs
+-- https://github.com/windwp/nvim-autopairs
+
+return {
+  'windwp/nvim-autopairs',
+  event = 'InsertEnter',
+>>>>>>> 5e258d2 (Move plugin examples from README to optional plugin files (#831)):lua/kickstart/plugins/autopairs.lua
   -- Optional dependency
   dependencies = { 'hrsh7th/nvim-cmp' },
   config = function()
--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,10 +1,14 @@
+<<<<<<< HEAD:lua/custom/plugins/neo-tree.lua
 return {
   'nvim-neo-tree/neo-tree.nvim',
   branch = 'v3.x',
+  version = '*',
+>>>>>>> 5e258d2 (Move plugin examples from README to optional plugin files (#831)):lua/kickstart/plugins/neo-tree.lua
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
+<<<<<<< HEAD:lua/custom/plugins/neo-tree.lua
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     --
   },
@@ -19,4 +23,20 @@ return {
     vim.keymap.set('n', '<leader>tb', ':Neotree toggle show buffers right<cr>', { desc = 'Neotree buffers' })
     vim.keymap.set('n', '<leader>tg', ':Neotree float git_status<cr>', { desc = 'Neotree git status' })
   end,
+=======
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+>>>>>>> 5e258d2 (Move plugin examples from README to optional plugin files (#831)):lua/kickstart/plugins/neo-tree.lua
 }
