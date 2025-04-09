bb48689878142975d9a9585a76ef22434ceb66e3
Liam Dyer <liamcdyer@gmail.com>
2025-04-09 17:25:57 -0400

    feat: switch nvim-cmp for blink.cmp (#1426)

--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -1,22 +1,8 @@
-<<<<<<< HEAD:lua/custom/plugins/autopairs.lua
-
-return {
-  'windwp/nvim-autopairs',
-=======
 -- autopairs
 -- https://github.com/windwp/nvim-autopairs
 
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
->>>>>>> 5e258d2 (Move plugin examples from README to optional plugin files (#831)):lua/kickstart/plugins/autopairs.lua
-  -- Optional dependency
-  dependencies = { 'hrsh7th/nvim-cmp' },
-  config = function()
-    require('nvim-autopairs').setup {}
-    -- If you want to automatically add `(` after selecting a function or method
-    local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-    local cmp = require 'cmp'
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-  end,
+  opts = {},
 }
